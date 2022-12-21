39b5cfdc7101ca4ba451b23a8dd93ee06f835525
thideve <107429305+thideve@users.noreply.github.com>
2022-12-21 11:42:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload() {
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+    canvas = createCanvas(1000,1000);
+"<div id='circle'>";
+ circle(70,180,40); 
+ "</div>";
+    rect(300,101,80,100);
+    rect(50,200,500,40);
+    ellipse(520,150,100,100);   
+}
+
